9793f254a4404d2f9be72d4d3f2cbc1dd01704f0
dynosoar101 <vtvo3@uci.edu>
2025-05-28 17:31:52 -0700

    added stdio to deps.h

--- include/deps.h
+++ include/deps.h
@@ -4,6 +4,7 @@
 #ifndef DEPS_H //defines DEPS_H if not already defined
 #define DEPS_H
 #include "fileIO.h" //this module is dependent on fileIO module
+#include <stdio.h> //for file operations
 
 void generateGraph(FILE *file); //function to generate the graph of the dependencies
 //takes in parsed file from fileIO parseSrs function
