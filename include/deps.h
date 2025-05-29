41eae5e96dffefb00c2137bffeeb798159636da6
dynosoar101 <vtvo3@uci.edu>
2025-05-28 21:21:02 -0700

    fixed header

--- include/deps.h
+++ include/deps.h
@@ -6,11 +6,11 @@
 #include "fileIO.h" //this module is dependent on fileIO module
 #include <stdio.h> //for file operations
 
-void generateGraph(FILE *file); //function to generate the graph of the dependencies
-//takes in parsed file from fileIO parseSrs function
+void generateGraph(DepNode *head, const char *filename); //function to generate the graph of the dependencies
+//takes in parsed linkedlist form parsed srs file and prints it
 //prints ascii graph to console
 
 void generateReport(DepNode *head, const char *filename); //function to generate the report of the dependencies
-//returns a report detailing all of the dependencies
+//returns a report detailing all of the dependencies as an md
 
 #endif //eof
